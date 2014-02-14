cbc4aefabce872544e600c793a380f6f2b5f7c8f
Dane Larson <daneglarson@Danes-MacBook-Pro.local>
2014-02-14 11:20:18 -0800

    Initial Commit

---
+++ Math Tutor Program/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Math Tutor Program
+//
+//  Created by Dane Larson on 2/14/14.
+//  Copyright (c) 2014 Dane Larson. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
